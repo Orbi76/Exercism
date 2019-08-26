fbc10bbd5ffcb6ae3ade2a5c934a17bfcdc55057
Orbi76bit <oorbangabor@gmail.com>
2019-08-26 10:52:42 +0100

    test lowercase

--- Exercism/java/isogram/src/main/java/IsogramChecker.java
+++ Exercism/java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,25 @@
+import java.util.*;
+
 class IsogramChecker {
 
-    boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isIsogram(String phrase) {
+		String s = "";
 
+		s = phrase.toLowerCase();
+		//System.out.println(s);
+		return (s.equals(phrase)) ? true : false;
+
+		/*
+		 * 
+		 * List<String> list = Arrays.asList("ter","vez","ni");
+		 * list.stream().distinct().forEach(System.out::println);
+		 * 
+		 * 
+		 * 
+		 * if (phrase == s) { return true; // } // return ("".equals(phrase)) ? true :
+		 * false;
+		 * 
+		 * } else return false; }
+		 */
+	}
 }
