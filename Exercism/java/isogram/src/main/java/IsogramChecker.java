717972b8860ade7c0f12f7cb717dd8b139f74373
Orbi76bit <oorbangabor@gmail.com>
2019-08-26 18:16:08 +0100

    2 doesnt work

--- Exercism/java/isogram/src/main/java/IsogramChecker.java
+++ Exercism/java/isogram/src/main/java/IsogramChecker.java
@@ -4,10 +4,36 @@ class IsogramChecker {
 
 	boolean isIsogram(String phrase) {
 		String s = "";
-
+		char[] ch = null;
+		Set<Character> set = new HashSet<>();
+		
+		
+//		char valami = "a";
 		s = phrase.toLowerCase();
-		//System.out.println(s);
-		return (s.equals(phrase)) ? true : false;
+		ch = s.toCharArray();
+//		phrase = phrase.toLowerCase();
+		
+//		System.out.println(ch);
+//		System.out.println(ch + "  " +s + " char ");
+		System.out.println(Arrays.toString(ch) + "  " +s);		
+//		System.out.println(s);
+
+		//		ch.stream().distinct().forEach(System.out::println);
+		
+/*		for (int i=0; i<ch.length; i++ ){
+			if (array[i] != array)
+			System.out.println(ch);
+*/			
+			
+		for (char element : ch) {
+			if (!set.add(element)) {
+				return false;
+			}
+		}
+		return true;
+		
+		
+//		return (s.equals(phrase)) ? true : false;
 
 		/*
 		 * 
