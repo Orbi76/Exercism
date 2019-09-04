4d796a839c726cb14c54c23461e104f9d0af3bc3
Orbi76bit <oorbangabor@gmail.com>
2019-09-04 22:23:24 +0100

    twofer done

--- Exercism/java/two-fer/src/main/java/Twofer.java
+++ Exercism/java/two-fer/src/main/java/Twofer.java
@@ -1,19 +1,7 @@
 class Twofer {
 	String twofer(String name) {
-
-/*		if (name == null) {
-			return "One for you, one for me.";
-		} else {
-			return "One for " + name + ", one for me.";
-		}
-*/		
-		
-//		return String.format("One for %s, one for me.", name == null ?"you" : name);
-	//	String result = (name == null) ? "One for you, one for me." : "One for " + name + ", one for me.";
-//		String result = "One for "+ (name == null) ? you :  name  +", one for me.";
-		String res = ("One for " + (((name == null) ? "you" : name )+ ", one for me."));
-		System.out.println(res);
+		String res = ("One for " + (((name == null) ? "you" : name) + ", one for me."));
 		return res;
 	}
-	
+
 }
